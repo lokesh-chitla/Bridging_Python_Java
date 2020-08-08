07771e27ef933e788981eaa56f94f57f93849332
Lokesh Chitla <53783706+lokesh-chitla@users.noreply.github.com>
2020-08-09 03:23:18 +0530

    Create MyWriteClass.java
    ~$ javac MyWriteClass.java
    ~$ java MyWriteClass
    First msg - Hey
    Second msg - Thanks!
    Yes
    ~$

---
+++ test2/MyWriteClass.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class MyWriteClass {
+        public static void main(String[] args) throws InterruptedException{
+                System.out.println("First msg - Hey");
+                System.out.println("Second msg - Thanks!");
+                System.out.println("Yes");
+        }
+}
